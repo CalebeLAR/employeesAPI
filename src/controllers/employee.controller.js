fa7931654007991e5456244b0d7e364bd4065c8d
calebeLAR <ca6ele@gmail.com>
2023-04-24 17:17:15 -0400

    Transactions

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -35,7 +35,35 @@ const getById = async (req, res) => {
   };
 }
 
+const insert = async (req, res) => {
+  const t = await sequelize.transaction();
+  try {
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+    return employee;
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
+    console.log(e);
+    throw e; // Jogamos o erro para a controller tratar
+  }
+};
+
 module.exports = {
   getAll,
-  getById
+  getById,
+  insert
 };
